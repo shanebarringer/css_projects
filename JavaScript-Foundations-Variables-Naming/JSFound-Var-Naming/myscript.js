b98d6272ea5a6a5dc3e28b34c83e2a796eb32010
Shane Barringer <shane.barringer@outlook.com>
2014-12-03 10:00:09 -0500

    collection of TeamTreehouse CSS projects

---
+++ JavaScript-Foundations-Variables-Naming/JSFound-Var-Naming/myscript.js
@@ -0,0 +1,46 @@
+/* JavaScript Foundations: Variables */
+
+var color = "red";
+
+var myDiv = document.getElementById('myDiv');
+myDiv.style.background = "black";
+myDiv.style.color = "#ffffff";
+
+// Starts with A-Z a-z _ $
+// Continue with A-Z a-z 0-9 _ $
+
+// Valid Variables
+var car = "Toyota";
+var Color = "blue";
+var _myVariable = "something";
+var $specialName = 1;
+var a58389 = "What is this?";
+
+
+// Invalid Variable Names
+// var 3colors = "red green blue";
+// var winning% = 30;
+// var person-name = "Jim";
+// var @you = "are awesome!";
+
+// var continue = true;
+
+
+var myVar;
+
+console.log(typeof myVar ==="undefined");
+console.log(myVar === undefined);
+
+var x = null;
+
+if(x) {
+	console.log("if")
+} else {
+	console.log("else")
+}
+
+
+
+
+
+
